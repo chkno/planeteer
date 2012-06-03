ebe808f6f0fed968fc1949da25b4c869ec98abf5
Scott Worley <sworley@chkno.net>
2012-06-03 04:09:18 -0700

    Fix test: Add trading dimension.

--- planeteer_test.go
+++ planeteer_test.go
@@ -3,8 +3,9 @@ package main
 import "testing"
 
 func TestEncodeDecode(t *testing.T) {
-	dims := []int{3, 2, 4, 17, 26, 15, 1, 2, 1}
-	for i := 0; i < 318240; i++ {   // Product of dims
+	dims := []int{3, 2, 4, 17, 26, 15, 2, 1, 2, 1}
+	var i int32
+	for i = 0; i < 636480; i++ { // Product of dims
 		addr := DecodeIndex(dims, i)
 		for j := 0; j < len(dims); j++ {
 			if addr[j] >= dims[j] {
